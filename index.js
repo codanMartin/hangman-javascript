b4cb28327568f7f7f83665ed7fd64ce5b88c556b
Dan Martin <codanmartin.25@gmail.com>
2025-07-21 17:57:02 +0300

    feat: implemented stage 2/8: let’s play a game

--- index.js
+++ index.js
@@ -1,18 +1,65 @@
-class UI {
-    printGameTitle() {
-        console.log("H A N G M A N")
+const input = require('sync-input');
+
+class HangmanGameInstance {
+    constructor(wordToGuess) {
+        this.wordToGuess = wordToGuess;
+        this.guessed = false;
     }
 
-    printGreetingMessage() {
-        console.log("The game will be available soon.")
+    guess(word) {
+        this.guessed = word === this.wordToGuess;
+        return this.guessed;
     }
 
-    run() {
-        this.printGameTitle()
-        this.printGreetingMessage()
+    getResultMessage() {
+        return this.guessed ? "You survived!" : "You lost!";
     }
 }
 
-const ui = new UI()
+class HangmanGameController {
+    constructor(ui) {
+        this.ui = ui;
+        this.secretWord = "python";
+    }
 
-ui.run()
+    playRound() {
+        const game = new HangmanGameInstance(this.secretWord);
+        const guess = this.ui.promptForGuess();
+        game.guess(guess);
+        this.ui.showResult(game.getResultMessage());
+    }
+}
+
+class UI {
+    showTitle() {
+        console.log("H A N G M A N");
+    }
+
+    showWelcomeMessage() {
+        console.log("The game will be available soon.");
+    }
+
+    promptForGuess() {
+        return input("Guess the word: ");
+    }
+
+    showResult(message) {
+        console.log(message);
+    }
+}
+
+class HangmanApp {
+    constructor() {
+        this.ui = new UI();
+        this.controller = new HangmanGameController(this.ui);
+    }
+
+    run() {
+        this.ui.showTitle();
+        this.ui.showWelcomeMessage();
+        this.controller.playRound();
+    }
+}
+
+const app = new HangmanApp();
+app.run();
